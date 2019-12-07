156fdc20d9d666795e159c9da5f42d79b6311d8f
Morteza Noshad <33552139+mrtnoshad@users.noreply.github.com>
2019-12-06 16:31:03 -0800

    Update add_role_info.sql

--- add_role_info.sql
+++ add_role_info.sql
@@ -1,6 +1,6 @@
 drop table if exists noshad_test.cohort_AL_user_role;
 create table noshad_test.cohort_AL_user_role as
-(select AL.* , MP.user_role  
+(select AL.* , MP.unique_role as user_role 
   from noshad_test.cohort_AL as AL
   left join noshad_test.user_map as MP
   on MP.prov_map_id=AL.user_deid)
