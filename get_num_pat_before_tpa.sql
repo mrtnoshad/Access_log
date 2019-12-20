2467aad2bbfd597138d4b1fbcf4b992a33d4cecb
Morteza Noshad <33552139+mrtnoshad@users.noreply.github.com>
2019-12-20 11:30:18 -0800

    Update get_num_pat_before_tpa.sql

--- get_num_pat_before_tpa.sql
+++ get_num_pat_before_tpa.sql
@@ -1,13 +1,22 @@
+
 --CREATE TABLE `noshad_test.ADT_cohort_test_to_be_del` as
 
-SELECT ADT_real_date.*, count(*) as num_tranx FROM
+SELECT ADT_real_date.*, count(*) as num_tranx , 
+  count(*)/ DATETIME_DIFF(ADT_real_date.time_out, ADT_real_date.time_in, MINUTE) as num_tranx_rate 
+
+FROM
 -- Generate ADT_cohort with actual times
-  (SELECT ADT.* except(time_in,time_out), DATETIME_SUB(ADT.time_in, INTERVAL TMP.JITTER_test DAY) as    time_in,  DATETIME_SUB(ADT.time_out, INTERVAL TMP.JITTER_test DAY) as time_out
+  (SELECT ADT.* except(time_in,time_out), DATETIME_SUB(ADT.time_in, INTERVAL TMP.JITTER_test DAY) as time_in,  
+  DATETIME_SUB(ADT.time_out, INTERVAL TMP.JITTER_test DAY) as time_out
+  
   FROM `noshad_test.ADT_cohort_jit` as ADT,
   `noshad_test.tmp_rnd_shift` as TMP
+  
   WHERE ADT.jc_uid=TMP.ANON_ID
+  
   ORDER BY ADT.jc_uid, ADT.pat_enc_csn_id_coded, time_in
   ) as ADT_real_date,
+
 -- Generate AL with actual times
   (SELECT AL.*, DATETIME_SUB(AL.access_time_jittered, INTERVAL TMP.JITTER_test DAY) as access_time_real
   FROM `shc_access_log.shc_access_log_de` as AL,
@@ -15,6 +24,7 @@ SELECT ADT_real_date.*, count(*) as num_tranx FROM
   WHERE AL.rit_uid=TMP.ANON_ID
   ORDER BY AL.rit_uid
   ) as AL_real_date
+
 WHERE ADT_real_date.jc_uid=AL_real_date.rit_uid 
   AND ADT_real_date.time_in < AL_real_date.access_time_real 
   AND AL_real_date.access_time_real < ADT_real_date.time_out
