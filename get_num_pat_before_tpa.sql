352dc79c288fa19bc7cb8c877a628bcd7dd9678d
Morteza Noshad <33552139+mrtnoshad@users.noreply.github.com>
2019-12-20 10:31:49 -0800

    Update get_num_pat_before_tpa.sql

--- get_num_pat_before_tpa.sql
+++ get_num_pat_before_tpa.sql
@@ -17,8 +17,9 @@ SELECT ADT_real_date.*, count(*) as num_tranx FROM
   ) as AL_real_date
 WHERE ADT_real_date.jc_uid=AL_real_date.rit_uid 
   AND ADT_real_date.time_in < AL_real_date.access_time_real 
-  AND ADT_real_date.time_out > AL_real_date.access_time_real
-GROUP BY ADT_real_date.jc_uid, ADT_real_date.pat_enc_csn_id_coded, ADT_real_date.department_id, 
+  AND AL_real_date.access_time_real < ADT_real_date.time_out
+GROUP BY ADT_real_date.jc_uid, ADT_real_date.pat_enc_csn_id_coded, 
+  ADT_real_date.department_id, ADT_real_date.tpaAdminTime, 
   ADT_real_date.time_in, ADT_real_date.time_out
 ORDER BY ADT_real_date.department_id
 
