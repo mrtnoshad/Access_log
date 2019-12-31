abb9fab5a758447f71d3dd36442e1724ff674b36
Morteza Noshad <33552139+mrtnoshad@users.noreply.github.com>
2019-12-31 13:54:31 -0800

    Update adt_summary.sql

--- adt_summary.sql
+++ adt_summary.sql
@@ -1,7 +1,7 @@
-drop table if exists noshad_test.adt_summary;
-create table noshad_test.adt_summary as
-(select ADT_in.jc_uid, ADT_in.pat_enc_csn_id_coded, ADT_in.department_id, ADT_in.in_event_type, 
-        ADT_out.out_event_type, ADT_in.event_time_jittered as in_time, ADT_out.event_time_jittered as out_time
+drop table if exists noshad.adt_summary;
+create table noshad.adt_summary as
+(select ADT_in.jc_uid, ADT_in.pat_enc_csn_id_coded, ADT_in.department_id, 
+    min(ADT_in.event_time_jittered) as in_time, max(ADT_out.event_time_jittered) as out_time
   
   from starr_datalake2018.adt as ADT_in,
        starr_datalake2018.adt as ADT_out
@@ -11,5 +11,8 @@ create table noshad_test.adt_summary as
     and ADT_in.department_id = ADT_out.department_id
     and ( ADT_in.in_event_type = 'Admission' or ADT_in.in_event_type = 'Transfer In')
     and ( ADT_out.out_event_type = 'Discharge' or ADT_out.out_event_type = 'Transfer Out')
+  GROUP BY ADT_in.jc_uid, ADT_in.pat_enc_csn_id_coded,  ADT_in.department_id
+  ORDER BY ADT_in.jc_uid, ADT_in.pat_enc_csn_id_coded, ADT_in.department_id
 )
+
 
