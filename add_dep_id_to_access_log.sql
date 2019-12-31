f5cc4e550bf81133408734ae8d5329939d114c11
Morteza Noshad <33552139+mrtnoshad@users.noreply.github.com>
2019-12-30 22:25:40 -0800

    Update add_dep_id_to_access_log.sql

--- add_dep_id_to_access_log.sql
+++ add_dep_id_to_access_log.sql
@@ -6,3 +6,9 @@ CREATE Table noshad.shc_access_log_de_dep_id AS
   WHERE  
     ADT.in_time < AL.access_time_jittered 
     AND AL.access_time_jittered  < ADT.out_time)
+    
+# Then run the following to remove the duplicate access log information
+CREATE TABLE `noshad.shc_access_log_de_dep_id_merged` AS
+(SELECT * Except (department_id), min(department_id) as department_id  FROM `som-nero-phi-jonc101.noshad.shc_access_log_de_dep_id` 
+GROUP BY rit_uid, process_id,metric_id, metric_name, user_deid,workstation_id,csn,access_action_c,access_action_name, deployment_id,
+audit_session_id, access_time_jittered_utc,access_time_jittered)
