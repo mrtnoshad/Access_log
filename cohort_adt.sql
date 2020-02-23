15b8c4acc9ac124419ef8d2bbd9bbd7e76c2fa3f
Morteza Noshad <33552139+mrtnoshad@users.noreply.github.com>
2020-02-22 17:56:12 -0800

    Update cohort_adt.sql

--- cohort_adt.sql
+++ cohort_adt.sql
@@ -1,18 +1,19 @@
 drop table if exists noshad_test.cohort_AL_adt;
-create table noshad_test.cohort_AL_adt as
+create table noshad.cohort_AL_adt as
 
 (select 
       ADT.jc_uid, ADT.pat_enc_csn_id_coded, ADT.department_id, ADT.in_event_type, 
         ADT.out_event_type, ADT.in_time, ADT.out_time
   
-  from noshad_test.adt_summary as ADT,
-       noshad_test.cohort_AL_60 as CH
+  from noshad.adt_summary as ADT,
+       noshad.cohort_AL_60 as CH
   
   where ADT.pat_enc_csn_id_coded in (CH.pat_enc_csn_id_coded)
   group by ADT.jc_uid, ADT.pat_enc_csn_id_coded, ADT.department_id, ADT.in_event_type, 
         ADT.out_event_type, ADT.in_time, ADT.out_time
   order by ADT.pat_enc_csn_id_coded, ADT.in_time
 )
+
 
 
 
