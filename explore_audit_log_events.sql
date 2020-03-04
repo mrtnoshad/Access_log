5b970aa2bbe4b8fd7b4c8783e04430f05b60e7e5
Morteza Noshad <33552139+mrtnoshad@users.noreply.github.com>
2020-03-04 13:58:34 -0800

    Update explore_audit_log_events.sql

--- explore_audit_log_events.sql
+++ explore_audit_log_events.sql
@@ -5,3 +5,13 @@ access_time_jittered <= tpaAdminTime
 and not metric_name like 'SmartLink%'
 GROUP BY metric_name 
 ORDER BY num desc --access_time_jittered 
+
+
+---- A grouped metric names:
+select mg.name, count(*)
+from `shc_access_log.shc_access_log_de` as al
+   join `shc_access_log.access_log_metric` using (metric_id)
+   join `shc_access_log.zc_metric_group` as mg using (metric_group_c)
+group by mg.name
+order by count(*) desc
+limit 100
